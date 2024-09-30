3fea48e32f48e66b378d6f0cf9ca88e8314a5c19
MATHEUS SILVA DA CRUZ <matheus_silva-cruz@estudante.sesisenai.org.br>
2024-09-30 09:57:45 -0300

    Cria Buscar e Adicionar User

--- index.js
+++ index.js
@@ -8,21 +8,44 @@ const nodemon = require('nodemon')
 const app = express()
 const port = 3000
 const path = require('path')
-const basePath= path.join(__dirname)
+const basePath= path.join(__dirname, 'templates')
 
-const checkAuth = function(req,res,next){
-    req.authStatus = true
-    if(req.authStatus){
-        console.log('Está Logado')
-    } else {
-        console.log('Não Está Logado')
-    }
-}
-    app.use(checkAuth)
+app.get('/users/add',(req,res) => {
+    res.sendFile(`${basePath}/form.html`)
+})
+
+app.post('/users/save', (req,res) => {
+    console.log(req.body)
+    const name = req.body.name
+    const email = req.body.email
+    console.log(name)
+    console.log(email)
+})
+
+app.get('/users/:id',(req,res) => {
+    const id = req.params.id
+    // leitura, resgatar um usuário do banco de dados
+    console.log(`Estamos buscando pelo usuário: ${id}`)
+    res.sendFile(`${basePath}/users.html`)
+})
+
 app.get('/',(req,res) => {
-    res.sendFile(`${basePath}/home.html`)
+    res.sendFile(`${basePath}/index.html`)
 }) //faz uma requisição e espera uma resposta dela
 
 app.listen(port, () => {
-    console.log(`App rodando na porta ${port}`)
+    console.log(`App rodando na porta: ${port}`)
 })
+
+// var checkAuth = function(req,res,next){
+//     req.authStatus = true
+
+//     if(req.authStatus){
+//         console.log('Está Logado')
+//         next()
+//     } else {
+//         console.log('Não Está Logado')
+//     }
+// }
+
+// app.use(checkAuth)
